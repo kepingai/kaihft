8c73a4f27c5cce12585256dc2d1efaf3b3d12fae
Diaz Agasatya <diazagasatya@kepingai.com>
2021-09-17 10:58:11 -0700

    package created #KAISIGNAL-169 add work Development 15m production-packaged

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="kaihft",
+    packages=[
+        "kaihft",
+        "kaihft.databases",
+        "kaihft.engines",
+        "kaihft.publishers",
+        "kaihft.services",
+        "kaihft.subscribers"
+    ],
+    version="0.0.1",
+    author="PT. Idabagus Engineering Indonesia",
+    author_email="support@kepingai.com",
+    maintainer="Ida Bagus Ratu Diaz Agasatya", 
+    maintainer_email="diazagasatya@kepingai.com",
+    description="This library will be used as a KepingAI Signal library for Signal Engine (Layer 1) production."
+)
