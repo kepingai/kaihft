d2bb7a33b43ae5f4544c1ed448685d71829cd61e
Diaz Agasatya <diazagasatya@kepingai.com>
2021-11-25 12:20:03 -0800

    add install requires

--- setup.py
+++ setup.py
@@ -11,6 +11,18 @@ setuptools.setup(
         "kaihft.services",
         "kaihft.subscribers"
     ],
+    install_requires=[
+        "firebase-admin==2.15.1",
+        "pandas==1.1.5",
+        "pandas-ta==0.2.23b0",
+        "asyncio==3.4.3",
+        "click==7.1.2",
+        "unicorn-binance-websocket-api==1.34.0",
+        "google-cloud-pubsub==2.8.0",
+        "python-binance==0.7.5",
+        "numpy==1.21.2",
+        "numexpr==2.7.1"
+    ],
     version="0.0.8",
     author="PT. Idabagus Engineering Indonesia",
     author_email="support@kepingai.com",
