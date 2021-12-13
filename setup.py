6790ab3396bb6402a7f0cfdd3c218d77df84481d
Diaz Agasatya <diazagasatya@kepingai.com>
2021-12-13 14:47:53 -0800

    new versiong

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
         "numpy==1.21.2",
         "numexpr==2.7.1"
     ],
-    version="0.0.9",
+    version="0.1.0",
     author="PT. Idabagus Engineering Indonesia",
     author_email="support@kepingai.com",
     maintainer="Ida Bagus Ratu Diaz Agasatya", 
