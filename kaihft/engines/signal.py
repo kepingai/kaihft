4966e4a7c395a4a7c6f8bf6a83d0b68e22333bdf
ernestrhutapea <ehutapea@kepingai.com>
2023-01-12 12:42:48 +0700

    ha direction for reversal seeking

--- kaihft/engines/signal.py
+++ kaihft/engines/signal.py
@@ -186,8 +186,10 @@ class Signal():
         # if the heikin-ashi direction differs from the signal direction,
         # proceed to close the signal as a stop loss (STOPPED)
         if ha_direction is not None:
-            if (self.direction == 1 and ha_direction == 1) or \
-                    (self.direction == 0 and ha_direction == -1):
+            long_ha_dir_close = 1 if self.ha_reverse else -1
+            short_ha_dir_close = -1 if self.ha_reverse else 1
+            if (self.direction == 1 and ha_direction == long_ha_dir_close) or \
+                    (self.direction == 0 and ha_direction == short_ha_dir_close):
                 self.update_realized_profit(status=SignalStatus.STOPPED)
                 logging.info(f"[ha-stopped] signal - symbol: {self.symbol}, "
                              f"direction: {self.direction}, realized-spread: "
